d60165a695eeb6cca31ba4c81d819f08ce46bf82
Dipjyoti Sikder <dip@bitmascot.com>
2021-07-10 13:46:26 +0600

    Add project files.

---
+++ Controllers/BaseController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BaseApp.Controllers
+{
+
+    [ApiController]
+    [Route("api/basis")]
+    public class BaseController : ControllerBase
+    {
+
+        [HttpGet]
+        [Route("first-index")]
+        public IActionResult Index()
+        {
+
+            return Ok("index result");
+        }
+
+        [HttpGet]
+        [Route("second-index")]
+        public IActionResult Index(int id)
+        {
+
+            return Ok("Not index result");
+        }
+
+        //[HttpGet]
+        //public IActionResult Index(int id, int uid)
+        //{
+
+        //    return Ok("NotIndexEver result");
+        //}
+
+
+        // Unique path to get something unique
+    }
+}
